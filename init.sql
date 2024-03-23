00f369295a83c53046509a92537397fcd6539277
Eduardo <eduguimara@gmail.com>
2024-03-23 11:04:55 -0300

    Add a simple security feature

--- init.sql
+++ init.sql
@@ -5,3 +5,6 @@ CREATE TABLE IF NOT EXISTS tb_product(
     price NUMERIC(7, 2) not null,
     quantity INTEGER not null
 );
+
+INSERT INTO tb_product (name, description, price, quantity) VALUES ('TV LG 50 polegadas OLED', 'TV show de bola com alta definição de imagem', 3500.49, 40);
+INSERT INTO tb_product (name, description, price, quantity) VALUES ('Geladeira Consulta', 'Geladeira smart com frost free', 5629.99, 20);
